2f316c9e8a2169a97e1a704f677258e1085d302a
BjornPython <110831700+BjornPython@users.noreply.github.com>
2022-10-02 19:02:37 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from TESTER_1 import *
+from pprint import pprint
+nathan = aiBrain()
+
+#print(nathan.record_food("I ate 2eggs, 100 grams of beef, and 200 grams of rice, and a chicken leg for breakfast", "efdvc"))
+
+print(nathan.record_cardio("I ran 20 miles and jogged for 15 minutes"))
